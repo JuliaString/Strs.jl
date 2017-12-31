60fb206cd2ef73354fe69aea27a001c17a2a59c9
ScottPJones <scottjones@alum.mit.edu>
2017-12-31 07:29:40 -0500

    Add benchmarking file

---
+++ test/bench.jl
@@ -0,0 +1,570 @@
+using Unicode
+using BenchmarkTools
+
+const test_legacy = true
+
+@static if test_legacy
+    using LegacyStrings
+end
+
+using StringLiterals
+using Strs
+import Strs: LineCounts, CharTypes, CharStat, calcstats
+
+create_vector(T, len) = Vector{T}(uninitialized, len)
+
+import Base: show
+
+if !isdefined(Base, :isascii)
+    import Base.Unicode: isascii
+end
+
+const userdir = "/Users/scott/"
+
+const dpath   = joinpath(userdir, "textsamples")
+const gutpath = joinpath(userdir, "gutenberg")
+const outpath = joinpath(userdir, "samples")
+const sampledir = outpath
+
+const gutenbergbooks =
+    (("files/2600/2600-0",        "English"), # War & Peace, some other languages in quotes
+     ("files/1400/1400-0",        "English"), # Great Expectations, uses Unicode quotes
+     ("files/42286/42286-0",      "Hungarian"),
+     ("files/32941/32941-0",      "Japanese"),
+     ("files/24264/24264-0",      "Chinese"),
+     ("files/40687/40687-0",      "Telugu"), # Third most spoken in India, official
+     ("files/50513/50513-0",      "French"),
+#     ("files/43007/43007-0",      "Arabic"),
+     ("cache/epub/38496/pg38496", "Portuguese"),
+     ("cache/epub/2000/pg2000",   "Spanish"), # Don Quijote
+     ("cache/epub/48750/pg48750", "Swedish"),
+     ("cache/epub/48322/pg48322", "German"),
+     )
+
+const downloadedbooks =
+    (("LYSAIa GORA DIeVICh'Ia - SIeRGIeI GOLOVAChIoV.txt", "Russian"),
+     )
+
+#Load books from Project Gutenberg site, removing lines added at beginning and end that
+# are not part of the book, as much as possible
+function load_gutenberg!(books, list, dict)
+    for (nam, lang) in list
+        cnt = get(dict, lang, 0)
+        dict[lang] = cnt + 1
+        outnam = cnt == 0 ? "$lang.txt" : "$lang-$cnt.txt"
+        lname = joinpath(gutpath, outnam)
+        download(joinpath("http://www.gutenberg.org/", nam * ".txt"), lname)
+        println("Saved to: ", lname)
+        lines = readlines(lname)
+        # Eliminate initial lines, empty lines, trailing lines
+        pos = 0
+        len = length(lines)
+        while (pos += 1) <= len
+            l = lines[pos]
+            sizeof(l) > 41 && startswith(l, "***") && endswith(l, "***") &&
+                contains(l, "START OF TH") && contains(l, " PROJECT GUTENBERG EBOOK") && break
+        end
+        out = Vector{String}()
+        while (pos += 1) <= len
+            l = lines[pos]
+            sizeof(l) > 41 && startswith(l, "***") && endswith(l, "***") &&
+                contains(l, "END OF TH") && contains(l, " PROJECT GUTENBERG EBOOK") && break
+            isempty(l) || push!(out, l)
+        end
+        push!(books, (outnam, out))
+    end
+    books
+end
+
+function load_books()
+    dict = Dict{String,Int}()
+    books = Vector{Tuple{String, Vector{String}}}()
+    for (nam, lang) in downloadedbooks
+        cnt = get(dict, lang, 0)
+        dict[lang] = cnt + 1
+        outnam = cnt == 0 ? "$lang.txt" : "$lang-$cnt.txt"
+        lines = readlines(joinpath(dpath, nam))
+        # Eliminate empty lines
+        pos = 0
+        len = length(lines)
+        out = Vector{String}()
+        while (pos += 1) <= len
+            l = lines[pos]
+            isempty(l) || push!(out, l)
+        end
+        push!(books, (outnam, out))
+    end
+    load_gutenberg!(books, gutenbergbooks, dict)
+end
+
+function save_books(books)
+    for (nam, book) in books
+        outnam = joinpath(outpath, nam)
+        open(outnam, "w") do io
+            for lin in book
+                println(io, lin)
+            end
+            println("Saved $nam")
+        end
+    end
+end
+
+#=
+books = load_books()
+save_books(books)
+=#
+
+show(io::IO, cnt::LineCounts) =
+    pr"\(io)\%10d(cnt.bytes)\%12.3f(cnt.bytes/cnt.chars)"
+
+function show(io::IO, s::CharTypes)
+    pr"\(io)\%10d(s.ascii)\%10d(s.latin)\%10d(s.utf2byte)\%10d(s.ucs2)"
+    pr"\(io)\%10d(s.utf32)\%10d(s.surr)\%10d(s.invalid)"
+end
+
+function show(io::IO, v::Tuple(String,CharStat})
+    s = v[2]
+    pr_ul(io, "File name              Lines     Chars   Avg C/L     Empty")
+    pr_ul(io, "       Min       Max   MaxType\n")
+    pr"\(io)\%-18s(v[1])\%10d(s.num)\%10d(s.len)\%10.3f(s.len/s.num)\%10d(s.empty)"
+    pr"\(io)\%10d(s.minlen)\%10d(s.maxlen)%10s(string(enctyp(s.maxtype)))\n\n"
+    pr_ul(io, "Character Types:       ")
+    pr_ul(io, "ASCII    Latin1    2-Byte      UCS2     UTF32 Surrogate   Invalid\n")
+    pr"\(io)Total characters: \(s.chars)\n"
+    pr"\(io)Lines with > 0:   \(s.lines)\n"
+end
+
+const pwc = print_with_color
+pwc(c, l) = pwc(c, STDOUT, l)
+
+pr_ul(l)  = pwc(:underline, l)
+
+print_size_ratio(io, val) =
+    pwc(val < .95 ? :green : val > 1.05 ? :red : :normal, io, f"\%6.3f(val)")
+print_time_ratio(io, val) =
+    pwc(val < .95 ? :red : val > 1.05 ? :green : :normal, io, f"\%10.3f(val)")
+
+print_size_ratio(val) = print_size_ratio(STDOUT, val)
+print_time_ratio(val) = print_time_ratio(STDOUT, val)
+
+print_ratio(val) =
+    pwc(val < .95 ? :green : val > 1.05 ? :red : :normal, STDOUT, f"\%12.3f(val)")
+
+function dispres(xres)
+    # (fname, stats, sizes, res)
+    (fname, stats, sizes, res) = xres
+    show(STDOUT, (fname, stats))
+    numchars = stats.len
+    maxlen = 0
+    pos = 0
+    for i = 1:length(res)
+        len = length(res[i][3])
+        len > maxlen && (maxlen = len; pos = i)
+    end
+    rs = res[pos][3]
+    pr_ul("\nType        B/Char")
+    for results in rs
+        pr_ul(f"\%10.10s(split(results[1])[1])")
+    end
+    pr"\nResults:          "
+    for results in rs
+        pr"\%10d(results[2])"
+    end
+    r1 = res[1]
+    t1 = r1[3]
+    pr"\n\n\%-12.12s(r1[1])\%6.3f(sizes[1]/numchars)"
+    for tim in t1
+        pr"\%10.3f(tim[3]/numchars)"
+    end
+    for i = 2:length(res)
+        rn = res[i]
+        pr"\n\%-12.12s(rn[1])\%6.3f(sizes[i]/numchars)"
+        #print_size_ratio(sizes[i]/numchars)
+        tn = rn[3]
+        minres = min(length(t1), length(tn))
+        for i = 1:minres
+            print_time_ratio(t1[i][3]/tn[i][3])
+        end
+        for i = minres+1:length(tn)
+            pwc(:yellow, f"\%10.3f(tn[i][3]/numchars)")
+        end
+    end
+    pr"\n"
+end
+
+const divline = string(repeat('#', 88),'\n','\f')
+
+function dispbench(totres)
+    for res in totres
+        dispres(res)
+        print(divline)
+    end
+end
+
+function countlength1(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines
+        cnt += length(text)
+    end
+    cnt
+end
+
+function countlength2(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines
+        cnt += sklength(text)
+    end
+    cnt
+end
+
+function countlength3(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines
+        cnt += oldlength(text)
+    end
+    cnt
+end
+
+function douppercase(lines::Vector{<:AbstractString})
+    cnt = 0
+    for (i, text) in enumerate(lines)
+        val = uppercase(text)
+        cnt += (val == text)
+    end
+    cnt
+end
+
+function countchars(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines, ch in text
+        cnt += 1
+    end
+    cnt
+end
+
+function countcps(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines, ch in codepoints(text)
+        cnt += 1
+    end
+    cnt
+end
+
+function countsize1(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines
+        cnt += sizeof(text)
+    end
+    cnt
+end
+
+function countsize2(lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines, cu in codeunits(text)
+        cnt += 1
+    end
+    cnt
+end
+
+function checkstr(fun, lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines
+        cnt += fun(text)
+    end
+    cnt
+end
+
+function checkchars(fun, lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines, ch in text
+        cnt += fun(ch)
+    end
+    cnt
+end
+
+function checkcp(fun, lines::Vector{<:AbstractString})
+    cnt = 0
+    for text in lines, cp in codepoints(text)
+        cnt += fun(cp)
+    end
+    cnt
+end
+
+function sumcharvals(lines::Vector{<:AbstractString})
+    t = 0
+    for text in lines, ch in text
+        t += UInt32(ch)
+    end
+    t
+end
+
+function sumcodepnts(lines::Vector{<:AbstractString})
+    t = 0
+    for text in lines, cp in codepoints(text)
+        t += convert(UInt32, cp)
+    end
+    t
+end
+
+function searchlines(lines, v)
+    t = 0
+    for text in lines
+        t += search(text, v)
+    end
+    t
+end
+
+
+function searchlines(lines::Vector{UniStr}, v)
+    t = 0
+    for text in lines
+        if typeof(text) == ASCIIStr
+            t += search(text::ASCIIStr, v)
+        elseif typeof(text) == _LatinStr
+            t += search(text::_LatinStr, v)
+        elseif typeof(text) == _UCS2Str
+            t += search(text::_UCS2Str, v)
+        else
+            t += search(text::_UTF32Str, v)
+        end
+    end
+    t
+end
+
+# maybe change all this to be table driven and use @eval!
+
+searchchar(lines::Vector{String}) = searchlines(lines, '\ufffd')
+searchchar(lines::Vector{T}) where {T} = searchlines(lines, codepoint(T)('\ufffd'))
+searchchar(lines::Vector{T}) where {T<:Union{ASCIIStr,LatinStr,_LatinStr}} =
+    searchlines(lines, ASCIIChr(0x1a))
+
+searchstr(lines::Vector{String})  = searchlines(lines, "thy")
+searchstr(lines::Vector{T}) where {T}  = searchlines(lines, T("thy"))
+
+# normalize
+# textwidth
+# isassigned
+
+sklength(str) = length(str)
+@inline function _length(s, i::Int, n::Int, c::Int)
+    i < n || return c
+    @inbounds b = codeunit(s, i)
+    @inbounds while true
+        while true
+            (i += 1) ≤ n || return c
+            0xc0 ≤ b ≤ 0xf7 && break
+            b = codeunit(s, i)
+        end
+        l = b
+        b = codeunit(s, i) # cont byte 1
+        c -= (x = b & 0xc0 == 0x80)
+        x & (l ≥ 0xe0) || continue
+
+        (i += 1) ≤ n || return c
+        b = codeunit(s, i) # cont byte 2
+        c -= (x = b & 0xc0 == 0x80)
+        x & (l ≥ 0xf0) || continue
+
+        (i += 1) ≤ n || return c
+        b = codeunit(s, i) # cont byte 3
+        c -= (b & 0xc0 == 0x80)
+    end
+end
+sklength(s::T) where {T<:AbstractString} = _length(s, 1, ncodeunits(s), ncodeunits(s))
+
+_is_surrogate_trail(c::Unsigned) = ((c & ~0x003ff) == 0xdc00)
+_is_valid_continuation(c) = ((c & 0xc0) == 0x80)
+
+function oldlength(str::T) where {T<:AbstractString}
+    dat = Vector{UInt8}(str)
+    cnt = 0
+    for i = 1:sizeof(dat)
+        @inbounds cnt += !_is_valid_continuation(dat[i])
+    end
+    cnt
+end
+function oldlength(s::UTF16Str)
+    dat = reinterpret(UInt16, s.data)
+    len = length(dat)
+    len == 0 && return 0
+    cnt = 0
+    for i = 1:len
+        @inbounds cnt += !_is_surrogate_trail(dat[i])
+    end
+    cnt
+end
+
+validstr(l)     = checkstr(isvalid, l)
+asciistr(l)     = checkstr(isascii, l)
+validchars(l)   = checkchars(isvalid, l)
+asciichars(l)   = checkchars(isascii, l)
+
+checkvalid(l)   = checkcp(isvalid,   l)
+checkascii(l)   = checkcp(isascii,   l)
+checkcntrl(l)   = checkcp(iscntrl,   l)
+checkdigit(l)   = checkcp(isdigit,   l)
+checkxdigit(l)  = checkcp(isxdigit,  l)
+checklower(l)   = checkcp(islower,   l)
+checkupper(l)   = checkcp(isupper,   l)
+checkalpha(l)   = checkcp(isalpha,   l)
+checknumeric(l) = checkcp(isnumeric, l)
+checkspace(l)   = checkcp(isspace,   l)
+checkalnum(l)   = checkcp(isalnum,   l)
+checkprint(l)   = checkcp(isprint,   l)
+checkpunct(l)   = checkcp(ispunct,   l)
+checkgraph(l)   = checkcp(isgraph,   l)
+
+function wrap(f, lines, cnts::LineCounts, t, msg, basetime=0%UInt)
+    tst = ""
+    try
+        res = f(lines)
+        raw = @belapsed ($f)($lines)
+        tim = round(UInt64, raw*1e9)
+        push!(t, (msg, res, tim))
+        pr"\%-22s(msg*':') \%12d(res)"
+        pr" \%12.3f(tim/1000000)"
+        pr" \%12.3f(tim/cnts.lines)"
+        pr" \%12.3f(tim/cnts.chars)"
+        pr" \%12.3f(tim/cnts.bytes)"
+        basetime != 0 && print_ratio(STDOUT, basetime/tim)
+        pr"\n"
+    catch ex
+        push!(t, (msg, 0, 0%UInt))
+        println(sprint(showerror, ex, catch_backtrace()))
+    end
+end
+
+# For each set of lines: save number of characters (should be the same for all)
+# For each test (currently just counting via Char iteration, length)
+
+const tests =
+    (
+     (countsize1,   "sizeof"),
+     (countlength1, "length"),
+     (countchars,   "iteration\nChar"),
+     (sumcharvals,  "sum\nchar vals"),
+     (asciistr,     "isascii\nstring"),
+     (validstr,     "isvalid\nstring"),
+     #=
+     (countlength2, "length\nSK"),
+     (countlength3, "length\nOld"),
+     (checkascii,   "isascii\nchars"),
+     (checkcntrl,   "iscntrl\nchars"),
+     (checkvalid,   "isvalid\nchars"),
+     (checklower,   "islower\nchars"),
+     (checkupper,   "isupper\nchars"),
+     (checkalpha,   "isalpha\nchars"),
+     (checkalnum,   "isalnum\nchars"),
+     (checkspace,   "isspace\nchars"),
+     (checkprint,   "isprint\nchars"),
+     (checkpunct,   "ispunct\nchars"),
+     (checkgraph,   "isgraph\nchars"),
+     (checkdigit,   "isdigit\nchars"),
+     (checkxdigit,  "isxdigit\nchars"),
+     (dolowercase,  "lowercase\nstring"),
+     (douppercase,  "uppercase\nstring"),
+     (dotitlecase,  "titlecase\nstring"),
+     =#
+    )
+
+function testperf(lines::Vector{T}, cnts, docnam, basetime) where {T<:AbstractString}
+    # Test performance
+    pr"""\%-22s(docnam) \%12s("Result") \%12s("ms total") """
+    pr"""\%12s("ns/line") \%12s("ns/char") \%12s("ns/byte")\n"""
+    t = []
+    # Make sure everything is already compiled
+    for (tst, nam) in tests
+        try
+            tst(lines)
+        catch ex
+            str = f"Failed test \(nam): \(sprint(showerror, ex, catch_backtrace()))"
+            println("File: ", docnam, ": ", str)
+            return docnam, 0, t, str
+        end
+    end
+    pos = 0
+    for (tst, nam) in tests
+        wrap(tst, lines, cnts, t, nam, basetime == nothing ? 0%UInt : basetime[pos += 1][3])
+    end
+    if T != String
+        wrap(countsize2,  lines, cnts, t, "iteration\ncodeunits")
+        #wrap(countcps,    lines, cnts, t, "iteration\ncp")
+        wrap(sumcodepnts, lines, cnts, t, "sum\ncp")
+    end
+    pr"\n"
+    docnam, cnts.bytes, t, ""
+end
+
+enctyp(t) = t == 1 ? ASCIIStr : t == 2 ? LatinStr : t < 5 ? UCS2Str : UTF32Str
+
+function benchdir(sampledir = defsampledir)
+    totres = []
+    totlines = []
+    totnames = []
+    totsizes = []
+    for fname in readdir(sampledir)
+        lines = readlines(joinpath(sampledir, fname))
+        stats = calcstats(lines)
+        show(STDOUT, (fname, stats))
+        list = [String, UniStr]
+        names = ["String", "UTF8Str", "UTF16Str", "UniStr"]
+        MT = enctyp(stats.maxtyp)
+        push!(list, MT)
+        push!(names, string(MT))
+        if isdefined(Main, :UTF8String)
+            push!(list, UTF8String, UTF16String, UTF32String)
+            push!(names, "UTF8String", "UTF16String", "UTF32String")
+        end
+        enc = []
+        numchars = stats.len
+        numlines = stats.num
+        push!(enc, lines)
+        for i = 2:length(list)
+            T = list[i]
+            vec = create_vector(T, numlines)
+            @inbounds for j = 1:numlines
+                vec[j] = convert(T, lines[j])
+            end
+            push!(enc, vec)
+        end
+        # Now calculate and display the size statistics for each
+        sizes = [sum(sizeof, enclines) for enclines in enc]
+        basesize = sizes[1]
+        pr"Types:      "
+        for nam in names ; pr"\%12s(nam)" ; end
+        pr"\nBytes:      "
+        for siz in sizes ; pr"\%12d(siz)" ; end
+        pr"\nBytes/Char: "
+        for siz in sizes ; pr"\%12.3f(siz/numchars)" ; end
+        pr"\nRelative:               "
+        for siz in sizes[2:end] ; print_ratio(siz/basesize) ; end
+        pr"\n\n"
+
+        # Now test the performance for each
+        res = Vector{Any}(uninitialized, length(list))
+        res[1] = testperf(enc[1], LineCounts(numlines, numchars, sizes[1]), names[1], nothing)
+        basetime = res[1][3]
+        for i = 2:length(list)
+            res[i] = testperf(enc[i], LineCounts(numlines, numchars, sizes[i]), names[i], basetime)
+        end
+        push!(totres, (fname, stats, sizes, res))
+
+        push!(totnames, names)
+        push!(totsizes, sizes)
+        push!(totlines, enc)
+    end
+    totres, totlines, totnames, totsizes
+end
+
+function saveresults(fname, res)
+    open(fname, "w") do io
+        serialize(io, res)
+    end
+end
+
+function loadresults(fname)
+    open(fname) do io
+        deserialize(io)
+    end
+end
+
