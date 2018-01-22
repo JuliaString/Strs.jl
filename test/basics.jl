38ca1226ada47356e1cfd24481cb061089b0cdc2
Stefan Schelm <stefan.schelm@runbox.com>
2018-01-22 13:45:04 +0100

    changed iterator names, typed dict keys

--- test/basics.jl
+++ test/basics.jl
@@ -5,16 +5,16 @@ test_string_length = 100
 string_types = [ASCIIStr, LatinStr]
 
 ##  create type specific test strings
-test_strings_base = Dict()
-for string_type in string_types
-    tmax = UInt32(typemax(Strs.codepoint_type(string_type)))
+test_strings_base = Dict{String, Any}()
+for T in string_types
+    tmax = UInt32(typemax(Strs.codepoint_type(T)))
     random_seq = rand(1:tmax, test_string_length)
     test_string = []
     for random_val in random_seq
         push!(test_string, Char(random_val))
     end
     test_string = join(test_string)
-    test_strings_base["$string_type"] = test_string
+    test_strings_base["$T"] = test_string
 end
 
 
@@ -24,19 +24,19 @@ test_strings_dict = Dict(
 )
 
 @testset "constructors" begin
-    for string_type in string_types
-        #@test convert(string_type, [0x61,0x62,0x63,0x21]) == "abc!"
-        #@test convert(string_type, "abc!") == "abc!"
+    for T in string_types
+        #@test convert(T, [0x61,0x62,0x63,0x21]) == "abc!"
+        #@test convert(T, "abc!") == "abc!"
 
-        emptystr = convert(string_type, "")
+        emptystr = convert(T, "")
         @test isempty(emptystr)
-        str_ab = convert(string_type, "ab")
-        str_abc = convert(string_type, "abc")
+        str_ab = convert(T, "ab")
+        str_abc = convert(T, "abc")
         @test start("abc") == 1
-        # let strs = [convert(string_type, ""),
-        #             convert(string_type, "a"),
-        #             convert(string_type, "a b c"),
-        #             convert(string_type, "до свидания")]
+        # let strs = [convert(T, ""),
+        #             convert(T, "a"),
+        #             convert(T, "a b c"),
+        #             convert(T, "до свидания")]
         #     for x in strs, y in strs
         #         @test (x === y) == (object_id(x) == object_id(y))
         #     end
@@ -46,10 +46,10 @@ end
 
 @testset "{starts,ends}with" begin
     i = 1
-    for string_type in string_types
-        test_strings = test_strings_dict["$string_type"]
+    for T in string_types
+        test_strings = test_strings_dict["$T"]
         for test_string in test_strings
-            converted_string = convert(string_type, test_string)
+            converted_string = convert(T, test_string)
             @test startswith(converted_string, test_string[1])
             @test !startswith(converted_string, test_string[end])
             @test endswith(converted_string, test_string[end])
