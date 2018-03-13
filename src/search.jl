f9f5e48c1d8e85b373095eb514ede76d6fd9e3ac
ScottPJones <scottjones@alum.mit.edu>
2018-03-13 10:17:18 -0400

    Add Regex support for byte Str types

--- src/search.jl
+++ src/search.jl
@@ -4,12 +4,11 @@
 export find_next, find_first, find_prev, find_last, found, find_result
 
 """
-    find_next(pattern::AbstractString, string::AbstractString, start::Integer)
-    find_next(pattern::Regex, string::String, start::Integer)
+    find_next(pattern::Union{Regex,AbstractString}, string::AbstractString, start::Integer)
 
 Find the next occurrence of `pattern` in `string` starting at position `start`.
 `pattern` can be either a string, or a regular expression, in which case `string`
-must be of type `String`.
+must be of type `String`, `ASCIIStr`, `UTF8Str` (or a `SubString` of one of those types)
 
 The return value is a range of indexes where the matching sequence is found, such that
 `s[find_next(x, s, i)] == x`:
@@ -32,8 +31,7 @@ julia> find_next("Julia", "JuliaLang", 2)
 function find_next end
 
 """
-    find_first(pattern::AbstractString, string::AbstractString)
-    find_first(pattern::Regex, string::AbstractString)
+    find_first(pattern::Union{Regex,AbstractString}, string::AbstractString)
 
 Find the first occurrence of `pattern` in `string`. Equivalent to
 [`find_next(pattern, string, 1)`](@ref).
@@ -76,7 +74,6 @@ function find_prev  end
 
 """
     find_last(pattern::AbstractString, string::AbstractString)
-    find_last(pattern::Regex, string::String)
 
 Find the last occurrence of `pattern` in `string`. Equivalent to
 [`find_last(pattern, string, lastindex(s))`](@ref).
@@ -86,7 +83,7 @@ Find the last occurrence of `pattern` in `string`. Equivalent to
 julia> find_last("o", "Hello to the world")
 15:15
 
-julia> find_first("Julia", "JuliaLang")
+julia> find_last("Julia", "JuliaLang")
 1:5
 ```
 """
@@ -103,7 +100,7 @@ find_result(::Type{<:AbstractString}, v) = v
 
 @static if VERSION < v"0.7.0-DEV"
 
-find_next(pat::Regex, str, pos::Integer) = search(str, pat, pos)
+find_next(pat::Regex, str::AbstractString, pos::Integer) = Base.findnext(str, pat, pos)
 
 export EqualTo, equalto, OccursIn, occursin
 
@@ -149,7 +146,8 @@ else
 
 import Base: EqualTo, equalto, OccursIn, occursin
 
-find_next(pat::Regex, str, pos::Integer) = coalesce(findnext(pat, str, pos), _not_found)
+find_next(pat::Regex, str::AbstractString, pos::Integer) =
+    coalesce(findnext(pat, str, pos), _not_found)
 
 #=
     nothing_sentinel(i) = i == 0 ? nothing : i
@@ -400,3 +398,131 @@ in(chr::AbsChar,   str::Str)            = contains(str, chr)
 in(pat::Str, str::Str)                  = contains(str, pat)
 in(pat::Str, str::AbstractString)       = contains(str, pat)
 in(pat::AbstractString, str::Str)       = contains(str, pat)
+
+using Base.PCRE
+
+using Base: DEFAULT_COMPILER_OPTS, DEFAULT_MATCH_OPTS
+import Base: Regex, match
+
+const ByteRegexCSE = Union{ASCIICSE,Latin_CSEs,Binary_CSEs}
+
+cvt_compile(::Type{<:ByteRegexCSE}, co) = co & ~PCRE.UTF
+cvt_match(::Type{<:ByteRegexCSE}, co)   = co & ~PCRE.NO_UTF_CHECK
+cvt_compile(::Type{UTF8CSE}, co)  = co | PCRE.NO_UTF_CHECK | PCRE.UTF
+cvt_match(::Type{UTF8CSE}, co)    = co | PCRE.NO_UTF_CHECK
+
+Regex(pattern::Str{C}, co, mo) where {C<:Byte_CSEs} =
+    Regex(String(pattern), cvt_compile(C, co), mo)
+Regex(pattern::Str{C}) where {C<:Byte_CSEs} =
+    Regex(String(pattern),
+          cvt_compile(C, DEFAULT_COMPILER_OPTS), cvt_match(C, DEFAULT_MATCH_OPTS))
+
+function _update_compiler_opts(flags)
+    options = DEFAULT_COMPILER_OPTS
+    for f in flags
+        options |= f=='i' ? PCRE.CASELESS  :
+                   f=='m' ? PCRE.MULTILINE :
+                   f=='s' ? PCRE.DOTALL    :
+                   f=='x' ? PCRE.EXTENDED  :
+                   throw(ArgumentError("unknown regex flag: $f"))
+    end
+    options
+end
+
+Regex(pattern::Str{C}, flags::AbstractString) where {C<:Byte_CSEs} =
+    Regex(pattern, cvt_compile(C, _update_compile_opts(flags)), cvt_match(C, DEFAULT_MATCH_OPTS))
+
+struct RegexMatchStr{T<:AbstractString}
+    match::T
+    captures::Vector{Union{Nothing,T}}
+    offset::Int
+    offsets::Vector{Int}
+    regex::Regex
+end
+
+function show(io::IO, m::RegexMatchStr)
+    print(io, "RegexMatchStr(")
+    show(io, m.match)
+    idx_to_capture_name = PCRE.capture_names(m.regex.regex)
+    if !isempty(m.captures)
+        print(io, ", ")
+        for i = 1:length(m.captures)
+            # If the capture group is named, show the name.
+            # Otherwise show its index.
+            capture_name = get(idx_to_capture_name, i, i)
+            print(io, capture_name, "=")
+            show(io, m.captures[i])
+            if i < length(m.captures)
+                print(io, ", ")
+            end
+        end
+    end
+    print(io, ")")
+end
+
+# Capture group extraction
+getindex(m::RegexMatchStr, idx::Integer) = m.captures[idx]
+function getindex(m::RegexMatchStr, name::Symbol)
+    idx = PCRE.substring_number_from_name(m.regex.regex, name)
+    idx <= 0 && error("no capture group named $name found in regex")
+    m[idx]
+end
+
+getindex(m::RegexMatchStr, name::AbstractString) = m[Symbol(name)]
+
+"""
+Check if the compile flags match the current search
+
+If not, free up old compilation, and recompile
+For better performance, the Regex object should hold spots for 5 compiled regexes,
+for 8-bit, UTF-8, 16-bit, UTF-16, and 32-bit code units
+"""
+function check_compile(::Type{C}, regex::Regex) where {C<:CSE}
+    # ASCII is compatible with all (current) types, don't recompile
+    re = regex.regex
+    C == ASCIICSE && re != C_NULL && return
+    oldopt = regex.compile_options
+    cvtcomp = cvt_compile(C, oldopt)
+    if cvtcomp != oldopt
+        regex.compile_options = cvtcomp
+        re == C_NULL || (PCRE.free_re(re); regex.regex = re = C_NULL)
+        regex.match_data == C_NULL ||
+            (PCRE.free_match_data(regex.match_data); regex.match_data = C_NULL)
+    end
+    if re == C_NULL
+        regex.regex = re = PCRE.compile(regex.pattern, cvtcomp)
+        PCRE.jit_compile(re)
+        regex.match_data = PCRE.create_match_data(re)
+        regex.ovec = PCRE.get_ovec(regex.match_data)
+    end
+    nothing
+end
+
+function _match(::Type{C}, re, str, idx, add_opts) where {C<:CSE}
+    check_compile(C, re)
+    PCRE.exec(re.regex, str, idx-1, cvt_match(C, re.match_options | add_opts), re.match_data) ||
+        return nothing
+    ovec = re.ovec
+    n = div(length(ovec),2) - 1
+    ss = C == _LatinCSE ? Str{LatinCSE}(str) : str
+    mat = SubString(ss, ovec[1]+1, prevind(ss, ovec[2]+1))
+    cap = Union{Nothing,SubString{typeof(ss)}}[ovec[2i+1] == PCRE.UNSET ? nothing :
+                                        SubString(ss, ovec[2i+1]+1,
+                                                  prevind(ss, ovec[2i+2]+1)) for i=1:n]
+    off = Int[ ovec[2i+1]+1 for i=1:n ]
+    RegexMatchStr(mat, cap, Int(ovec[1]+1), off, re)
+end
+
+match(re::Regex, str::Str, idx::Integer, add_opts::UInt32=UInt32(0)) =
+    _match(cse(str), re, str, Int(idx), add_opts)
+
+function find_next(re::Regex, str::Union{Str{C},SubString{<:Str{C}}},
+                   idx::Integer) where {C<:Byte_CSEs}
+    if idx > ncodeunits(str)
+        @boundscheck boundserr(str, idx)
+        return nothing
+    end
+    check_compile(C, re)
+    (PCRE.exec(re.regex, str, idx-1, cvt_match(C, re.match_options), re.match_data)
+     ? ((Int(re.ovec[1])+1):prevind(str,Int(re.ovec[2])+1)) : nothing)
+end
