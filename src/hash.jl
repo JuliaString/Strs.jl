4e11de181d278ba6fb0890c0b018e2ec3b2c31bc
ScottPJones <scottjones@alum.mit.edu>
2018-03-20 11:07:04 -0400

    Further simplification of murmurhash functions

--- src/hash.jl
+++ src/hash.jl
@@ -62,16 +62,17 @@ utf8crc(str::Union{S,SubString{S}},
 _memhash(siz, ptr, seed) =
     ccall(Base.memhash, UInt, (Ptr{UInt8}, Csize_t, UInt32), ptr, siz, seed % UInt32)
 
-_hash(str, seed)      = first(murmurhash128(str, seed%UInt32)) + seed
-_hash(len, pnt, seed) = first(murmurhash128(len, pnt, seed%UInt32)) + seed
+_hash(seed)           = last(mmhash_128(seed%UInt32)) + seed
+_hash(str, seed)      = last(mmhash_128(str, seed%UInt32)) + seed
+_hash(len, pnt, seed) = last(mmhash_128(len, pnt, seed%UInt32)) + seed
 
 # Optimize conversion to ASCII or UTF8 to calculate compatible hash value
                           
 function hash(str::Union{S,SubString{S}}, seed::UInt) where {S<:Str}
     seed += Base.memhash_seed
+    (len = _len(str)) == 0 && return _hash(seed)
     @preserve str begin
-        len, pnt = _lenpnt(str)
-        len == 0 && return _hash(0, pnt, seed)
+        pnt = _pnt(str)
         len, flags, num4byte, num3byte, num2byte, latin1 = count_chars(S, pnt, len)
         # could be UCS2, _UCS2, UTF32, _UTF32, Text2, Text4
         _hash((flags == 0
@@ -83,19 +84,20 @@ end
 
 function hash(str::Union{S,SubString{S}}, seed::UInt) where {S<:Str{<:Latin_CSEs}}
     seed += Base.memhash_seed
+    (len = _len(str)) == 0 && return _hash(seed)
     @preserve str begin
-        len, pnt = _lenpnt(str)
-        (len != 0 && (cnt = count_latin(len, pnt)) != 0
+        pnt = _pnt(str)
+        ((cnt = count_latin(len, pnt)) != 0
          ? _hash(_latin_to_utf8(pnt, len + cnt), seed)
-         : _hash(len, pnt, seed)
+         : _hash(len, pnt, seed))
     end
 end
 
 function hash(str::Union{S,SubString{S}}, seed::UInt) where {S<:Str{UTF16CSE}}
     seed += Base.memhash_seed
+    (len = _len(str)) == 0 && return _hash(seed)
     @preserve str begin
-        len, pnt = _lenpnt(str)
-        len == 0 && return _hash(0, pnt, seed)
+        pnt = _pnt(str)
         len, flags, num4byte, num3byte, num2byte, latin1 = count_chars(S, pnt, len)
         _hash((flags == 0
                ? _cvtsize(UInt8, pnt, len)
@@ -108,5 +110,4 @@ end
 
 hash(str::Union{S,SubString{S}},
      seed::UInt) where {S<:Str{<:Union{ASCIICSE,UTF8CSE,Text1CSE,BinaryCSE}}} =
-    _hash(str, seed + Base.memhash_seed)
-
+         isempty(str) ? _hash(seed + Base.memhash_seed) : _hash(str, seed + Base.memhash_seed)
