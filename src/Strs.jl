2f09248410a1a440021450ceded994720276664e
ScottPJones <scottjones@alum.mit.edu>
2018-06-21 00:49:32 -0400

    Bump MIT version

--- src/Strs.jl
+++ src/Strs.jl
@@ -8,6 +8,7 @@ Licensed under MIT License, see LICENSE.md
 module Strs
 
 using ModuleInterfaceTools
+using ModuleInterfaceTools: @api, m_eval, cur_mod
 export @api, V6_COMPAT
 
 using PCRE2
@@ -15,14 +16,14 @@ using PCRE2
 import StrTables: lookupname, matchchar, matches, longestmatches, completions
 export lookupname, matchchar, matches, longestmatches, completions
 
-@api extend! StrRegex, StrLiterals
-@api use! Format, StrFormat, StrEntities
+@api extend! StrRegex, StrLiterals, Format
+@api use! StrFormat, StrEntities
 
 using InternedStrings: intern
-export @i_str, intern
+export @i_str, @I_str, intern
 
-macro i_str(str) ; Expr(:call, :intern, s_interp_parse(false, UniStr, str)) ; end
-macro I_str(str) ; Expr(:call, :intern, s_interp_parse(true,  UniStr, str)) ; end
+macro i_str(str) ; Expr(:call, :intern, interpolated_parse(str, StrBase._str)) ; end
+macro I_str(str) ; Expr(:call, :intern, interpolated_parse(str, StrBase._str, true)) ; end
 
 const m_eval = ModuleInterfaceTools.m_eval
 
@@ -32,7 +33,7 @@ end
 
 # Need to fix ModuleInterfaceTools to do this!
 for mod in (StrRegex, StrLiterals), grp in (:modules, :public, :public!)
-    m_eval(Expr( :export, getfield(m_eval(mod, :__api__), grp)...))
+    m_eval(cur_mod(), Expr( :export, getfield(m_eval(mod, :__api__), grp)...))
 end
 
 @api freeze
