b4753bb2e0053f6fc70208c98d73eab19dedc08e
ScottPJones <scottjones@alum.mit.edu>
2019-04-14 14:25:49 -0400

    Change export -> reexport to fix parsing issue, update version

--- src/Strs.jl
+++ src/Strs.jl
@@ -2,14 +2,13 @@ __precompile__(true)
 """
 Strs package
 
-Copyright 2017-018 Gandalf Software, Inc., Scott P. Jones
+Copyright 2017-2019 Gandalf Software, Inc., Scott P. Jones
 Licensed under MIT License, see LICENSE.md
 """
 module Strs
 
 using ModuleInterfaceTools
-using ModuleInterfaceTools: m_eval
-export @api, V6_COMPAT
+export @api
 
 using PCRE2
 
@@ -25,12 +24,7 @@ export @i_str, @I_str, intern
 macro i_str(str) ; Expr(:call, :intern, interpolated_parse(str, StrBase._str)) ; end
 macro I_str(str) ; Expr(:call, :intern, interpolated_parse(str, StrBase._str, true)) ; end
 
-const curmod = @static V6_COMPAT ? current_module() : @__MODULE__
-
-# Need to fix ModuleInterfaceTools to do this!
-for mod in (StrRegex, StrLiterals), grp in (:modules, :public, :public!)
-    m_eval(curmod, Expr( :export, getfield(m_eval(mod, :__api__), grp)...))
-end
+@api reexport StrRegex, StrLiterals
 
 @api freeze
 
